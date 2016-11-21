989c11121673eb0b8bb36a848629f40b031585f9
Mark Goho <markgoho@gmail.com>
2016-11-21 12:11:44 -0500

    JS and CSS optimized

--- gulpfile.js
+++ gulpfile.js
@@ -10,7 +10,7 @@ var gulp        = require('gulp'),
     del 		= require('del'),
     csso		= require('gulp-csso');
 
-gulp.task("concatScripts", function() {
+gulp.task("concatJS", function() {
 	return gulp.src([
 			 'js/fastclick.js',
 			 'js/foundation.js',
@@ -22,6 +22,23 @@ gulp.task("concatScripts", function() {
 		   .pipe(concat('app.js'))
 		   .pipe(maps.write('./'))
 		   .pipe(gulp.dest('js'));
+});
+
+gulp.task("concatCSS", function() {
+	return gulp.src([
+			 'css/normalize.css',
+			 'css/foundation.css',
+			 'css/basics.css',
+			 'css/menu.css',
+			 'css/hero.css',
+			 'css/photo-grid.css',
+			 'css/modals.css',
+			 'css/footer.css'
+		   ])
+		   .pipe(maps.init())
+		   .pipe(concat('styles.css'))
+		   .pipe(maps.write('./'))
+		   .pipe(gulp.dest('css'));
 });
 
 gulp.task("minifyScripts", function() {
@@ -38,8 +55,8 @@ gulp.task('minifyCSS', function() {
 			   .pipe(gulp.dest('css'));
 });
 
-gulp.task("deploy", ['concatScripts', 'minifyScripts', 'compileSass'], function() {
-	return gulp.src(['css/main.css', 'js/app.min.js', 'index.html', 'img/**', 'fonts/**'], { base: './'})
+gulp.task("deploy", ['concatJS', 'minifyScripts', 'concatCSS', 'minifyCSS'], function() {
+	return gulp.src(['css/styles.min.css', 'js/app.min.js', 'index.html', 'img/**'], { base: './'})
 			   .pipe(gulp.dest('dist'));
 });
 
